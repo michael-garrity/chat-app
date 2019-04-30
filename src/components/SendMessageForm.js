2ea85b57a4f82475666167e1ccab4d093c6b9411
MikeRGarrity <mikergarrity@gmail.com>
2019-04-29 22:24:44 -0500

    adding state, event listeners, and event handlers to SendMessageForm component

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,10 +1,37 @@
 import React from "react";
 
 class SendMessageForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.message);
+    /* send off message */
+  }
+
   render() {
     return (
-      <form className="send-message-form">
-        <input placeholder="SendMessageForm" type="text" />
+      <form onSubmit={this.handleSubmit} className="send-message-form">
+        <input
+          onChange={this.handleChange}
+          value={this.state.message}
+          placeholder="Type your message and hit enter"
+          type="text"
+        />
       </form>
     );
   }
