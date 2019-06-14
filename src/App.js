38852233979c6fc40ccce8392d6736a24c832b1d
unknown <mikergarrity@gmail.com>
2019-06-14 18:20:36 -0500

    added functionality for newRoomForm component

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     this.sendMessage = this.sendMessage.bind(this);
     this.subscribeToRoom = this.subscribeToRoom.bind(this);
     this.getRooms = this.getRooms.bind(this);
+    this.createRoom = this.createRoom.bind(this)
   }
 
   componentDidMount() {
@@ -78,6 +79,14 @@ class App extends React.Component {
     });
   }
 
+  createRoom(name) {
+    this.currentUser.createRoom({
+      name
+    })
+    .then(room => this.subscribeToRoom(room.id))
+    .catch(err => console.log('error with createRoom: ', err))
+  }
+
   render() {
     console.log("this.state.messages:", this.state.messages);
     return (
@@ -86,9 +95,13 @@ class App extends React.Component {
           roomId={this.state.roomId}
           subscribeToRoom={this.subscribeToRoom}
           rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]} />
-        <MessageList messages={this.state.messages} />
-        <SendMessageForm sendMessage={this.sendMessage} />
-        <NewRoomForm />
+        <MessageList 
+          roomId={this.state.roomId}
+          messages={this.state.messages} />
+        <SendMessageForm 
+          disabled={!this.state.roomId}
+          sendMessage={this.sendMessage} />
+        <NewRoomForm createRoom={this.createRoom} />
       </div>
     );
   }
